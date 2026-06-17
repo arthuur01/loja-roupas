ebd4d127119a13e327a2e53172855782a234d5c2
arthurlink0901@gmail.com <arthurlink0901@gmail.com>
2026-06-17 13:04:15 -0300

    adiconando crud para os tamanhos

---
+++ app/crud/tamanhos/components/post_form.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+
+export default function BotaoAdicionar(){
+    const [mostrar, setMostrar] = useState(false);
+
+    async function adicionarCategoria(formData: FormData){
+
+        const nome = formData.get("nome");
+
+        await fetch("/api/tamanhos", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({nome}),
+        });
+        location.reload();
+    }
+
+    return(
+        <>
+         <h1 onClick= {() => setMostrar(!mostrar)} className="text-5xl h-30 flex justify-center items-center cursor-pointer hover:text-blue-500">
+            +
+         </h1>
+        {mostrar && (
+            <form action={adicionarCategoria} className="flex justify-center">
+                <input autoFocus name="nome" placeholder="Nome do novo tamanho"className="border p-2 rounded"/>
+            </form>
+            )}
+        </>
+    );
+}
