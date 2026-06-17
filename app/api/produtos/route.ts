08c8a8018cb4ee485ad522c3d7d6ddc031323934
arthurlink0901@gmail.com <arthurlink0901@gmail.com>
2026-06-17 14:01:08 -0300

    adicionando prisma e integrando com banco de dados

---
+++ app/api/produtos/route.ts
@@ -0,0 +1,45 @@
+import db from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    /*Pq rows está em [ ] desestruturação de arrays, é a mesma coisa que 
+        const resultado = await db.query("SELECT * FROM produtos");
+        const rows = resultado[0];
+       rows → contém os registros retornados pelo banco.
+        O segundo elemento (resultado[1]) → contém informações das colunas (metadados).
+    
+    */
+    const [rows] = await db.query("SELECT * FROM produtos");
+    return NextResponse.json(rows);
+}
+
+export async function POST(req: Request){
+    const {nome} = await req.json();
+    await db.query(
+        "INSERT INTO produtos(nome) VALUES(?)",
+        [nome]
+    );
+    return NextResponse.json({
+        success: true,
+    });
+}
+
+export async function PUT(req: Request){
+    const {novo_nome, antigo_nome} = await req.json();
+    await db.query("UPDATE produtos SET nome = ? WHERE nome = ?",
+        [novo_nome, antigo_nome]
+    );
+    return NextResponse.json({
+        success: true,
+    });
+}
+
+export async function DELETE(req: Request){
+    const {nome} = await req.json();
+    await db.query("DELETE FROM produtos WHERE nome = ?",
+        [nome]
+    );
+    return NextResponse.json({
+        success: true,
+    });
+}
